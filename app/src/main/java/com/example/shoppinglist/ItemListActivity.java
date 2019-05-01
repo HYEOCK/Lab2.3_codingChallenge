3c6d58019786cc6156bfd4ca00d349c30fa319a8
gracefulcanada@gmail.com <gracefulcanada@gmail.com>
2019-05-01 11:49:05 -0700

    Finished Shopping List coding challenge

--- app/src/main/java/com/example/shoppinglist/ItemListActivity.java
+++ app/src/main/java/com/example/shoppinglist/ItemListActivity.java
@@ -1,10 +1,13 @@
 package com.example.shoppinglist;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -12,12 +15,14 @@ import java.util.ArrayList;
 public class ItemListActivity extends AppCompatActivity {
     public static final String ITEMS_EXTRA = "ITEMS_EXTRA";
     private ArrayList<String> items;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_list);
         items = new ArrayList<>();
+        mLocationEditText = findViewById(R.id.location_edittext);
     }
 
     public void itemSelected(View view) {
@@ -32,5 +37,16 @@ public class ItemListActivity extends AppCompatActivity {
         replyIntent.putStringArrayListExtra(ITEMS_EXTRA, items);
         setResult(RESULT_OK, replyIntent);
         finish();
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }else {
+            Log.d("ImplicitIntents", "Can't handle this intent");
+        }
     }
 }
